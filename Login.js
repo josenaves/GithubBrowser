6c20f19daea3d6075145f98a8295ccce881f25dd
José Naves <josenaves@gmail.com>
2016-04-05 23:40:55 -0300

    Add button, image, logo, textinputs

--- Login.js
+++ Login.js
@@ -1,17 +1,71 @@
 'use strict';
 
 import React, {
-  Text, Component
+  Text, TextInput, Component, StyleSheet, View, Image, TouchableHighlight
 } from 'react-native'; 
 
 
 class Login extends Component {
 	render() {
 		return (
-			<Text style={{color: 'black'}}>Hello</Text>
-		);
+			<View style={styles.container}>
+				<Image style={styles.logo}
+					source={require('./assets/Octocat.png')} />
 
+				<Text sytle={styles.heading}>
+					Github browser
+				</Text>
+				<TextInput style={styles.input} 
+					placeholder="Github username" />
+				<TextInput style={styles.input} 
+					placeholder="Github password" />					
+
+				<TouchableHighlight style={styles.button}>
+					<Text style={styles.buttonText}>Log in</Text>
+				</TouchableHighlight>
+
+			</View>
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#F5FCFF',
+		flex: 1,
+		paddingTop: 40,
+		alignItems: 'center',
+		padding: 10
+	},
+	logo: {
+		width: 66,
+		height: 55,
+	},
+	heading: {
+		fontSize: 30,
+		marginTop: 10
+	}, 
+	input: {
+		height: 50, 
+		marginTop: 10,
+		padding: 4, 
+		fontSize: 18,
+		borderWidth: 1,
+		borderColor: '#48bbec'
+	}, 
+	button: {
+		height: 50,
+		backgroundColor: '#48BBEC',
+		alignSelf: 'stretch',
+		marginTop: 10,
+		justifyContent: 'center'
+	},
+	buttonText: {
+		fontSize: 22,
+		color: '#FFF',
+		alignSelf: 'center'
+	}
+});
+
 
 module.exports = Login;
